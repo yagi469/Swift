31db1b1b06439f27225a64ac958db3a4f1da7fbe
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-06-22 12:02:50 +0900

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -4,11 +4,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        // print("Hello World")
+        print(type(of: 2))
+        print(type(of: 2.0))
         
-        // print(type(of: 2))
-        // print(type(of: 2.0))
+        var string = "Hello World"
+        print(string)
+        string = "Hello"
+        print(string)
+        
+        var hello = "こんにちは"
+        var name = "太郎"
+        var string3 = hello + " " + name + "さん"
+        print(string3)
+        print("\(hello) \(name)さん")
+        
+        let int = 2
+        let result = int + Int(2.1)
+        print(result)
+        result = Double(int) + 2.1
+        print(result)
+        
+        let string = "666"
+        int = 777
+        result = Int(string)! + int
+        print(result)
     }
 }
