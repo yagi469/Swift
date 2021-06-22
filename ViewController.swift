5111f5870ae5f3991ac07843785633824401797f
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-06-22 11:53:32 +0900

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -7,5 +7,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // print("Hello World")
+        
+        // print(type(of: 2))
+        // print(type(of: 2.0))
     }
 }
